d058719bfd4d54a33833bdc90c16743244e298bf
“nataliu” <13636424061@163.com>
2017-01-11 11:08:47 +0800

    jobs_controller crud & resources :jobs

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,48 @@
+before_action :authenticate_user!, only:[:new, :create, :edit, :updata, :destroy]
+
+  def index
+    @ jobs = Job.all
+  end
+
+  def new
+    @ job = Job.new
+  end
+
+  def edit
+    @ job = Job.find(params[:id])
+  end
+
+  def show
+    @ job = Job.find(params[:id])
+  end
+
+  def create
+    @ job = Job.new(job_params)
+    if @job.save
+       redirect_to jobs_path
+    else
+       render :new
+    end
+  end
+
+  def udpate
+    @ job = Job.find(params[:id])
+    if @job.update
+       redirect to jobs_path(job_params), alert : "update successfully"
+    else
+       render :edit
+    end
+  end
+
+  def destroy
+    @ job = Job.find(params[:id]), warning: "deleted"
+    @ job.destroy
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit[:title, :description]
+  end
+
+end 
